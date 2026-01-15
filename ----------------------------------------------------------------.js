01c2bc5c271e320c8310d701770c0cf17cef63b4
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2026-01-15 16:36:27 +0900

    Update ----------------------------------------------------------------.js

--- ----------------------------------------------------------------.js
+++ ----------------------------------------------------------------.js
@@ -1,1 +1,2 @@
-
+console.console = console;
+console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.console.log("------------------------------------------------------------------------------------------------------------");
